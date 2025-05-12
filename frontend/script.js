fe62c15db1ac44ab530919f4aab71b7376a27ec9
stephon Bridges <stephonjbridges@gmail.com>
2025-05-11 22:31:25 -0400

    feat: default show code blocks expanded

--- frontend/script.js
+++ frontend/script.js
@@ -70,7 +70,7 @@ async function askJunius() {
     const res = await fetch(endpoint, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ question, age })
+      body: JSON.stringify({ question, age, history })
     });
 
     if (!res.ok) {
